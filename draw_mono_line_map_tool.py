f8c180195bc176bc59fb63f1d001c6f46a13b263
SartineCat® <jobryant@163.com>
2020-08-04 16:48:34 +0800

    add snapping for azimuth measurement

--- draw_mono_line_map_tool.py
+++ draw_mono_line_map_tool.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 from qgis.PyQt.QtCore import pyqtSignal, QSettings
 from qgis.PyQt.QtGui import QColor
-from qgis.core import Qgis, QgsGeometry, QgsPoint, QgsWkbTypes
-from qgis.gui import QgsMapToolEmitPoint, QgsRubberBand
+from qgis.PyQt.QtCore import Qt
+from qgis.core import Qgis, QgsGeometry, QgsPoint, QgsWkbTypes, QgsSettings
+from qgis.gui import QgsMapToolEmitPoint, QgsRubberBand, QgsVertexMarker
 
 
 class DrawMonoLineMapTool(QgsMapToolEmitPoint):
@@ -28,6 +29,7 @@ class DrawMonoLineMapTool(QgsMapToolEmitPoint):
         self.rubberBand.setWidth(1)
         # self.rubberBand.setLineStyle(Qt.DashLine)
         self.points = []
+        self.vertex = None
         self.reset()
 
     def reset(self):
@@ -39,9 +41,11 @@ class DrawMonoLineMapTool(QgsMapToolEmitPoint):
     def canvasPressEvent(self, e):
         self.isEmittingPoint = False
 
+
     def canvasReleaseEvent(self, e):
         self.isEmittingPoint = True
-        self.startPoint = self.toMapCoordinates(e.pos())
+        pt = self.snappoint(e.originalPixelPoint())
+        self.startPoint = pt
         if len(self.points) < 2:
             self.rubberBandDraw.reset(QgsWkbTypes.LineGeometry)
             self.rubberBand.reset(QgsWkbTypes.LineGeometry)
@@ -58,6 +62,7 @@ class DrawMonoLineMapTool(QgsMapToolEmitPoint):
             self.isEmittingPoint = False
 
     def canvasMoveEvent(self, e):
+        self.snappoint(e.originalPixelPoint()) # input is QPoint
         if not self.isEmittingPoint:
             return
         self.endPoint = self.toMapCoordinates(e.pos())
@@ -74,9 +79,31 @@ class DrawMonoLineMapTool(QgsMapToolEmitPoint):
     def activate(self):
         self.reset()
         super(DrawMonoLineMapTool, self).activate()
+        self.snapcolor = QgsSettings().value( "/qgis/digitizing/snap_color" , QColor( Qt.magenta ) )
         self.activated.emit()
 
     def deactivate(self):
         self.reset()
         super(DrawMonoLineMapTool, self).deactivate()
+        self.removeVertexMarker()
         self.deactivated.emit()
+
+    def removeVertexMarker(self):
+        if self.vertex is not None:
+            self.canvas.scene().removeItem(self.vertex)
+            self.vertex = None
+
+    def snappoint(self, qpoint):
+        match = self.canvas.snappingUtils().snapToMap(qpoint)
+        if match.isValid():
+            if self.vertex is None:
+                self.vertex = QgsVertexMarker(self.canvas)
+                self.vertex.setIconSize(12)
+                self.vertex.setPenWidth(2)
+                self.vertex.setColor(self.snapcolor)
+                self.vertex.setIconType(QgsVertexMarker.ICON_BOX)
+            self.vertex.setCenter(match.point())
+            return (match.point()) # Returns QgsPointXY
+        else:
+            self.removeVertexMarker()
+            return self.toMapCoordinates(qpoint) # QPoint input, returns QgsPointXY
